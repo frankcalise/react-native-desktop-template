8aa720b1f622e3558808fd224948a8e29c828d85
Frank Calise <fcalise@gmail.com>
2022-12-19 16:31:26 -0500

    feat: windows target

---
+++ windows/ReactNativeDesktopTemplate/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ReactNativeDesktopTemplate::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ReactNativeDesktopTemplate::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
